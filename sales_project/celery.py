6f78e1ae4f799b82a857bbd4257eae2b11704f0e
wsrn829 <wsrn829@gmail.com>
2024-02-05 01:31:58 -0500

    Update celery.py

--- sales_project/celery.py
+++ sales_project/celery.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from celery.schedules import crontab
+from sales_project import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sales_project.settings')
 
@@ -9,3 +11,9 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 
+app.conf.beat_schedule = {
+    'poll-task': {
+        'task': 'poller.poll',  # Task to be scheduled
+        'schedule': 60.0,  # Set the schedule (every 60 seconds in this example)
+    },
+}
